ced763ed82ec6aa5455769b3e7a304bda678ffe5
Jess Rascal <me@jessrascal.com>
2024-03-06 19:46:15 +0000

    Tailwind config: Add base text colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import { gray } from 'tailwindcss/colors';
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
@@ -6,8 +9,13 @@ export default {
       colors: {
         primary: 'rgb(253, 40, 74)',
         secondary: 'rgb(119, 53, 255)',
+        // for text colours
+        high: gray[700],
+        medium: gray[600],
+        low: gray[400],
+        disabled: gray[300],
       },
     },
   },
   plugins: [],
-}
+};
