2419bdbe70af1c5b05a3c4947a6b5c9c502d4ad7
Jess Rascal <me@jessrascal.com>
2024-03-06 21:33:32 +0000

    Add BaseTable component

---
+++ src/components/base/BaseTable.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps({
+  columnHeaders: {
+    type: Array,
+    required: true,
+  },
+  dataName: {
+    type: String,
+    default: 'rows',
+  },
+});
+</script>
+
+<template>
+  <div class="border rounded overflow-hidden">
+    <table class="table-auto w-full md:text-sm text-left text-medium">
+      <thead class="text-medium uppercase bg-gray-200">
+        <tr>
+          <th v-for="(header, index) in columnHeaders" :key="index" class="px-4 py-2 font-medium">
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody class="divide-y">
+        <slot name="rows">
+          <tr>
+            <td :colspan="columnHeaders.length" class="px-4 py-4 text-low italic">No {{ dataName }} to display</td>
+          </tr>
+        </slot>
+      </tbody>
+    </table>
+  </div>
+</template>
