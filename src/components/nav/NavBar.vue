0ca40f6a98c073d5013835502b2ec3c116513683
Jess Rascal <me@jessrascal.com>
2024-03-06 19:52:41 +0000

    NavBar: Hide logo on small screens

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -5,7 +5,7 @@ import NavItem from '@/components/nav/NavItem.vue';
 
 <template>
   <nav class="navbar">
-    <NavLogo class="nav-logo" />
+    <NavLogo class="nav-logo hidden md:block" />
     <NavItem to="/" label="People" />
     <NavItem to="/settings" label="Settings" is-disabled />
   </nav>
@@ -18,11 +18,12 @@ import NavItem from '@/components/nav/NavItem.vue';
   padding: 1rem;
   display: flex;
   align-items: center;
-  column-gap: 2rem;
+  column-gap: 1.5rem;
 
   /* medium screens (md) */
   @media screen and (min-width: 768px) {
     padding-inline: 2rem;
+    column-gap: 2rem;
   }
 
   .nav-logo {
