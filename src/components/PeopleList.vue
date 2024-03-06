10e30c2513da256c03cbd60abb203472f74dd02c
Jess Rascal <me@jessrascal.com>
2024-03-06 17:59:26 +0000

    Initial commit

---
+++ src/components/PeopleList.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>People list placeholder</div>
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PeopleList from '@/components/PeopleList.vue'
+</script>
+
+<template>
+  <main>
+    <PeopleList />
+  </main>
+</template>
